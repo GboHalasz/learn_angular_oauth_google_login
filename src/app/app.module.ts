43cc66e361f5dc6c8c168ade4fc64e1427f6b8f1
GboHalasz <hal2297dev@gmail.com>
2023-02-19 17:32:50 +0100

    Generate EntrancePage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntrancePageComponent } from './entrance-page/entrance-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrancePageComponent
   ],
   imports: [
     BrowserModule,
