9d7f3c77a13ff45e7fbf0d6f1cf11daff032e97d
Gabor Halasz <hal2297@gmail.com>
2023-02-19 17:50:52 +0100

    Add EntrancePage component to routes, set invalid routes to '/'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrancePageComponent } from './entrance-page/entrance-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: EntrancePageComponent, pathMatch: 'full'},
+
+{path: "**", redirectTo: "/"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
