0d64495f41a8871e8706404da3950cd81c843586
Gabor Halasz <hal2297@gmail.com>
2023-02-22 13:35:39 +0100

    import http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { OAuthModule } from 'angular-oauth2-oidc';
@@ -15,7 +16,8 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
   ],
   imports: [    
     BrowserModule,      
-    AppRoutingModule,
+    AppRoutingModule,    
+    HttpClientModule,
     OAuthModule,  
   ],
   providers: [],
