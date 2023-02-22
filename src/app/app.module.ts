fcce6a51ab0b004ad19a2f6d3386b15f0cf3ed7f
GboHalasz <hal2297dev@gmail.com>
2023-02-22 12:53:48 +0100

    oauth module add to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +13,10 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
     EntrancePageComponent,
     LoginpageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [    
+    BrowserModule,      
+    AppRoutingModule,
+    OAuthModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
