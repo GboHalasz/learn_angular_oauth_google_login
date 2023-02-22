21085d4202b50e8cf8b02b18b52b850e204c2ae5
Gabor Halasz <hal2297@gmail.com>
2023-02-22 11:06:00 +0100

    Add the login component, html-content and js scripts from the login project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EntrancePageComponent } from './entrance-page/entrance-page.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 const routes: Routes = [
   {path: "", component: EntrancePageComponent, pathMatch: 'full'},
+  {path: "login", component: LoginpageComponent},
 
 {path: "**", redirectTo: "/"}
 ];
---
+++ src/typings.d.ts
@@ -0,0 +1,4 @@
+declare function regValidation():any;
+declare function storeData():any;
+declare function userData():any;
+declare var myWelcome:any;
