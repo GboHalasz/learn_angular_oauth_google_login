a85c1bdb4838442181b8dd4c34206838b269ad68
GboHalasz <hal2297dev@gmail.com>
2025-06-12 15:05:53 +0200

    Remove unnecessary console.log statements

--- src/assets/mymodules/user.js
+++ src/assets/mymodules/user.js
@@ -1,12 +1,11 @@
 
 const userData = () => {
-    console.log("lefut az userdata")
     const user = {
         name: ""
     }
 
     const setNameFromSessionStorage = () => {
-       console.log(JSON.parse(sessionStorage.getItem("user")))
+
         user.name = JSON.parse(sessionStorage.getItem("user")) ? JSON.parse(sessionStorage.getItem("user")).name : "";
     }
 
