0855f33d9b60a9bbc426538dd3333bdfc7c50332
GboHalasz <hal2297dev@gmail.com>
2025-06-13 14:34:15 +0200

    Update scrollTo() to focus target element after smooth scroll:
    - adds delayed focus on a specific input element after scrolling into view.

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -1,25 +1,29 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { GoogleApiService } from '../google-api-service.service';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {GoogleApiService} from '../google-api-service.service';
 
 @Component({
-    selector: 'app-loginpage',
-    templateUrl: './loginpage.component.html',
-    styleUrls: ['./loginpage.component.css'],
-    standalone: false
+  selector: 'app-loginpage',
+  templateUrl: './loginpage.component.html',
+  styleUrls: ['./loginpage.component.css'],
+  standalone: false
 })
 export class LoginpageComponent {
   constructor(
     private router: Router,
     private readonly google: GoogleApiService
-  ) { }
+  ) {
+  }
+
+  @ViewChild('signUpSection') signUpSection!: ElementRef;
+  @ViewChild('signUpUser') signUpEmail!: ElementRef;
 
   async ngOnInit() {
     console.log('Starting Application!');
     await this.google.checkLoginStatus();
-    const { startVal, validData } = regValidation();
-    const { storeInSessionStr } = storeData();
-    const { user } = userData();
+    const {startVal, validData} = regValidation();
+    const {storeInSessionStr} = storeData();
+    const {user} = userData();
 
     if (user.name) {
       await this.router.navigate(['main'])
@@ -35,7 +39,11 @@ export class LoginpageComponent {
     })
   }
 
-  scrollTo(element: any): void {
-    (document.getElementById(element) as HTMLElement).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  scrollToSectionAndFocus(): void {
+    this.signUpSection.nativeElement.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
+    setTimeout(() => {
+      this.signUpEmail.nativeElement.focus();
+    }, 500);
   }
 }
